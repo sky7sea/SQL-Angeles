c10659644f5fb618008b126240fb9304838030ce
teamsmiley <teamsmiley@gmail.com>
2018-10-19 11:02:07 -0700

    asdf

--- devops/Study-04/gitlab-vm/init-server.sh
+++ devops/Study-04/gitlab-vm/init-server.sh
@@ -12,6 +12,8 @@ usermod -aG docker vagrant
 
 systemctl enable docker
 
+systemctl start docker
+
 curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
@@ -22,6 +24,9 @@ systemctl disable firewalld
 # selinux off
 sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 
+cd /data/docker/gitlab
+
+docker-compose up -d
 
 
 
